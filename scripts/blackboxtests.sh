6d211e5da4da5bc62e1d57ebc9929d0b0c23eb42
Bari Ballew <bari.ballew@nih.gov>
2019-08-23 14:52:39 -0400

    simulate empty interval file in test data

--- scripts/blackboxtests.sh
+++ scripts/blackboxtests.sh
@@ -18,9 +18,9 @@ echo "inputDir: '${myInPath}'" >> ${myOutPath}/TESTconfig.yaml
 echo "outputDir: '${myOutPath}'" >> ${myOutPath}/TESTconfig.yaml
 echo "logDir: '${myOutPath}/logs/'" >> ${myOutPath}/TESTconfig.yaml
 echo "tempDir: '/ttemp/bballew/${DATE}/'" >> ${myOutPath}/TESTconfig.yaml
-echo "intervalFile: '/DCEG/Projects/Exome/SequencingData/BED_FILES/seqcap_EZ_Exome_v3_v3utr_intersect_correct.intervals'" >> ${myOutPath}/TESTconfig.yaml
-echo "bedFile: '/DCEG/Projects/Exome/SequencingData/BED_FILES/seqcap_EZ_Exome_v3_v3utr_intersect_correct.bed'" >> ${myOutPath}/TESTconfig.yaml
-echo "clusterMode: 'qsub -q research.q -V -j y -o ${myOutPath}/logs/'" >> ${myOutPath}/TESTconfig.yaml
+echo "intervalFile: '/DCEG/CGF/Bioinformatics/Production/Bari/Germline_pipeline_v4_dev/germlineCallingV4/tests/regions/seqcap_EZ_Exome_v3_v3utr_intersect_correct_NOchr4.intervals'" >> ${myOutPath}/TESTconfig.yaml
+echo "bedFile: '/DCEG/CGF/Bioinformatics/Production/Bari/Germline_pipeline_v4_dev/germlineCallingV4/tests/regions/seqcap_EZ_Exome_v3_v3utr_intersect_correct_NOchr4.bed'" >> ${myOutPath}/TESTconfig.yaml
+echo "clusterMode: 'qsub -q long.q -V -j y -o ${myOutPath}/logs/'" >> ${myOutPath}/TESTconfig.yaml
 echo "snakePath: '/DCEG/CGF/Bioinformatics/Production/Bari/Germline_pipeline_v4_dev/germlineCallingV4/scripts/'" >> ${myOutPath}/TESTconfig.yaml
 echo "gatkPath: '/DCEG/Projects/Exome/SequencingData/GATK_binaries/gatk-4.0.11.0/'" >> ${myOutPath}/TESTconfig.yaml
 echo "refGenome: '/DCEG/CGF/Bioinformatics/Production/Bari/refGenomes/hg19_canonical_correct_chr_order.fa'" >> ${myOutPath}/TESTconfig.yaml
@@ -32,6 +32,6 @@ echo "modelPath: '/opt/wes/model.ckpt' # either /opt/wgs/model.ckpt or /opt/wes/
 
 module load python3/3.6.3 sge
 unset module
-cmd="qsub -q research.q -V -j y -S /bin/sh -o ${myOutPath} /DCEG/CGF/Bioinformatics/Production/Bari/Germline_pipeline_v4_dev/germlineCallingV4/scripts/submit.sh ${myOutPath}/TESTconfig.yaml"
+cmd="qsub -q long.q -V -j y -S /bin/sh -o ${myOutPath} /DCEG/CGF/Bioinformatics/Production/Bari/Germline_pipeline_v4_dev/germlineCallingV4/scripts/submit.sh ${myOutPath}/TESTconfig.yaml"
 echo "Command run: $cmd"
 eval $cmd
